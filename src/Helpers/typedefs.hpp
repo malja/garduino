cd77f79986febe8ee192a55ae39aa3620aa97a77
malja <looorin@gmail.com>
2018-12-10 19:08:55 +0100

    Removed unused typedefs

--- src/Helpers/typedefs.hpp
+++ src/Helpers/typedefs.hpp
@@ -1,19 +1,6 @@
 #ifndef TYPEDEFS_HPP
 #define TYPEDEFS_HPP
 
-#include <Arduino.h>
-
-/**
- * Number of milliseconds. 
- */
 typedef unsigned long long time_ms;
-
-/**
- * Unsigned and signed integers. 
- */
-typedef uint8_t     u8;
-typedef int8_t      s8;
-typedef uint32_t    u32;
-typedef int32_t     s32;
 
 #endif
